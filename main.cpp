70252ddef616553c727327cd0fa02000faf19822
alexaaldea <alexandra.aldea2004@gmail.com>
2023-12-05 12:14:56 +0200

    inceput proiect

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include "structs.h"
 
 int main()
 {
---
+++ structs.h
@@ -0,0 +1,12 @@
+#pragma once
+#define MAX 102
+
+struct nod
+{
+	char tip[MAX];
+	int val;
+	int x, y;
+	nod *urm;
+	///nod* ant;
+};
+
