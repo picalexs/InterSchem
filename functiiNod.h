e45aecf74c28feebd321c9eaaea2f24c4eb90047
picalexs <picalexs@gmail.com>
2023-12-06 20:51:23 +0200

    Functions

--- functiiNod.h
+++ functiiNod.h
@@ -1,29 +1,48 @@
 #pragma once
 #include "structs.h"
 #include <string>
+#include <vector>
 #include <iostream>
 
 using namespace std;
 
 void initNod(nod &N) {
-	N.tip.clear();
 	N.expresie.clear();
+	N.tip = 0;
 	N.x = 0; N.y = 0;
 	N.st = NULL; N.dr = NULL;
 }
 
-bool esteNodNull(nod &N) {
-	return N.tip.empty();
-}
-
-nod *creareNod() {
-	nod *N=new nod;
+nod* creareNod() {
+	nod* N = new nod;
 	initNod(*N);
 	return N;
 }
 
+void stergereNod(nod* N) {
+	if (N == nullptr)
+		return;
+	stergereNod(N->st);
+	stergereNod(N->dr);
+
+	N = nullptr;
+	delete N;
+}
+
+bool esteVerificare(nod& N) {
+	return (N.tip==0)
+}
+
+void inserareNod(nod &tata, bool fiu) {
+	nod* N = creareNod();
+	if (!fiu) {//bool fiu: 0-fiu st., 1-fiu dr.
+
+	}
+}
+
 void initArbore(arbore &A, string expresieNoua) {
-	nod *nodNou = creareNod();
+	nod *radacinaNoua = creareNod();
+	A.radacina = radacinaNoua;
 	A.radacina->expresie = expresieNoua;
 	A.nrNoduri = 1;
 }
@@ -32,7 +51,10 @@ bool esteArboreNull(arbore& A) {
 	return (A.radacina == NULL);
 }
 
-arbore* creareArbore() {
+arbore* creareArbore(string expresieNoua) {
 	arbore* A = new arbore;
-	initArbore(*A);
+	if(A==nullptr)
+		return;
+	initArbore(*A,expresieNoua);
+	return A;
 }
--- structs.h
+++ structs.h
@@ -1,17 +1,12 @@
 #pragma once
 #include <string>
 #include <iostream>
-
 using namespace std;
-
-struct punctLegatura {
-	int x, y;
-};
 
 struct nod
 {
-	string tip;
 	string expresie;
+	char tip;//0-START, 1-STOP, V-Atribuire Variabila, I-If, C-Citire, A-Afisare
 	int x = 0, y = 0;
 	nod* st = NULL, * dr = NULL;
 };
